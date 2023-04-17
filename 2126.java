92570eec70e9f2fdebf1e99bc29b46cea9d969d9
Ujjwal <88238259+ug2002@users.noreply.github.com>
2023-04-18 01:08:48 +0530

    Update 2126.java

--- 2126.java
+++ 2126.java
@@ -1,1 +1,14 @@
-
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        Arrays.sort(asteroids);
+        long mASS = (long)mass;
+        for(int i=0; i<asteroids.length; i++){
+            long aSTEROIDS=(long)asteroids[i];
+            if(mASS < aSTEROIDS)return false;
+            else{
+                mASS = mASS + aSTEROIDS;
+            }
+        }
+        return true;
+    }
+}
