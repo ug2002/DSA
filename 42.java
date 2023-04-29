4645ff73d3dba4672fd5983a519116cbbbc4621d
Ujjwal <88238259+ug2002@users.noreply.github.com>
2023-04-29 21:27:01 +0530

    42.java

--- 42.java
+++ 42.java
@@ -1,7 +1,19 @@
 class Solution {
-    public int trap(int[] height) {
-                
-       
-        
+    public int trap(int[] height) {      
+        int n = height.length;
+        int res = 0, l = 0, r = n - 1;
+        int rMax = height[r], lMax = height[l];
+        while (l < r) {
+            if (lMax < rMax) {
+                l++;
+                lMax = Math.max(lMax, height[l]);
+                res += lMax - height[l];
+            } else {
+                r--;
+                rMax = Math.max(rMax, height[r]);
+                res += rMax - height[r];
+            }
+        }
+        return res;
     }
 }
