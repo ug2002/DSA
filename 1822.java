51d8778eab37c7e835c72eb42360fe9e07df6a7e
Ujjwal <88238259+ug2002@users.noreply.github.com>
2023-05-02 23:19:04 +0530

    1822.java

---
+++ 1822.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int arraySign(int[] nums) {
+        int c=0;
+        for(int i=0;i<nums.length;i++){
+          if(nums[i]==0)return 0;
+          if(nums[i]<0)c++;
+        }
+        if(c%2 == 0)return 1;
+        return -1;
+    }
+}  
