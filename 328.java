bcde252c1a49fa869e1c8db9a2e4102377ea2d88
Ujjwal <88238259+ug2002@users.noreply.github.com>
2023-06-13 19:19:07 +0530

    328.java

---
+++ 328.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if(head==null || head.next==null || head.next.next==null)return head;
+        ListNode origHead = head, even =head, odd = head.next, oddStart = head.next;
+        while(even.next !=null && odd.next!=null){
+            even.next = odd.next;
+            even = even.next;
+            odd.next = even.next;
+            odd = odd.next;
+        }
+        even.next = oddStart;
+        return origHead;
+    }
+}
+// 2->3->6->7
+// 1=5=4
